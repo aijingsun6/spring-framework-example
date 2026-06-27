279cfae8c5c9eae80d21830f42e76f3c514ac4ad
alking <aijingsun6@gmail.com>
2026-06-27 15:24:04 +0800

    first commit

---
+++ src/main/java/org/alking/example/spring/Main.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2012-2023. All rights reserved.
+ */
+
+package org.alking.example.spring;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Main
+ *
+ * @author s30025793
+ * @since 2023/10/20
+ */
+public class Main {
+
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+    public static void main(String[] args) {
+        logger.info("main...");
+
+
+
+    }
+
+}
